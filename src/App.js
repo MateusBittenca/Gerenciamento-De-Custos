a5beb9039afe4c877fff632c92fde6e50befbf8f
MateusBittenca <mpbittenc@gmail.com>
2025-01-06 17:20:49 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Navbar from "./layout/Navbar";
+import Contato from "./components/pages/Contato";
+import NovoProjeto from "./components/pages/NovoProjeto";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path = "/contato" element = {<Contato />} /> 
+          <Route path = "/projeto" element = {<NovoProjeto />} /> 
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
+    
     </div>
   );
 }
