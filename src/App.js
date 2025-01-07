b70c7548e3431d7a6ee2c439a11fe8203ba20be0
MateusBittenca <mpbittenc@gmail.com>
2025-01-07 17:38:18 -0300

    css v2

--- src/App.js
+++ src/App.js
@@ -3,21 +3,37 @@ import Home from "./components/pages/Home";
 import Navbar from "./layout/Navbar";
 import Contato from "./components/pages/Contato";
 import NovoProjeto from "./components/pages/NovoProjeto";
+import Projeto from "./components/pages/Projeto";
+import Empresa from "./components/pages/Empresa";
+
+
+import Footer from "./layout/Footer";
 
 
 function App() {
   return (
     <div className="App">
    
+
     <BrowserRouter>
+  
       <Routes>
         <Route path="/" element={<Navbar />}>
           <Route index element={<Home />} />
           <Route path = "/contato" element = {<Contato />} /> 
-          <Route path = "/projeto" element = {<NovoProjeto />} /> 
+          <Route path = "/NovoProjeto" element = {<NovoProjeto />} /> 
+          <Route path="/projeto" element = {<Projeto/>}/>
+          <Route path="/empresa" element = {<Empresa/>}/>
         </Route>
       </Routes>
+     
     </BrowserRouter>
+   
+
+
+    <Footer/>
+
+
 
     
 
--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -5,21 +5,28 @@ import styles from './Navbar.module.css'
 
 function Navbar() {
     return (
-        <>
-          
+        <>   
             <nav className={styles.Navbar}>
                 <ul className= {styles.list}>
                     <li className= {styles.item}>
-                        <FaHouseUser />
+                        <FaHouseUser className= {styles.icon} />
                         <Link to="/">Home</Link>
                     </li>
                     <li className= {styles.item}>
-                        <FaPhone />
+                        <FaHouseUser className= {styles.icon} />
+                        <Link to="/empresa">Empresa</Link>
+                    </li>
+                    <li className= {styles.item}>
+                        <FaPhone className= {styles.icon} />
                         <Link to="/contato">Contato</Link>
                     </li>
                     <li className= {styles.item}>
-                        <FaPlus />
-                        <Link to="/projeto">Novo projeto</Link>
+                        <FaPlus className= {styles.icon} />
+                        <Link to="/NovoProjeto">Novo projeto</Link>
+                    </li>
+                    <li className= {styles.item}>
+                        <FaPlus className= {styles.icon} />
+                        <Link to="/Projeto">Projeto</Link>
                     </li>
                 </ul>
             </nav>
