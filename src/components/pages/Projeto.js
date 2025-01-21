7f7d0a68a0a10341d9ffeadd0563fb71b0d8dabc
MateusBittenca <mpbittenc@gmail.com>
2025-01-20 22:03:38 -0300

    adcionei layout aos cads e atribui a opção de excluir projeto aos cards tambem

--- src/components/pages/Projeto.js
+++ src/components/pages/Projeto.js
@@ -5,6 +5,7 @@ import { useLocation } from "react-router-dom"
 
 import Message from "../../layout/Message"
 import Container from "../../layout/Container"
+import Loading from "../../layout/Loading"
 import LinkButton from "../../layout/LinkButton"
 import ProjectCard from "../project/ProjectCard"
 import { useState, useEffect } from "react"
@@ -13,6 +14,8 @@ import { useState, useEffect } from "react"
 function Projeto() {
 
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
 
 
     const location = useLocation()
@@ -31,10 +34,28 @@ function Projeto() {
             .then((data) => {
                 console.log(data)
                 setProjects(data)
+                setRemoveLoading(true)
             })
             .catch((err) => console.log(err))
 
     }, [])
+
+    function removeProject(id) {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then(resp => resp.json())
+            .then((data) => {
+                console.log(data)
+                const novoProjeto = projects.filter(project => project.id !== id)
+                setProjects(novoProjeto)
+                setProjectMessage('Projeto excluído com sucesso')
+            })
+            .catch((err) => console.log(err))
+    }
+
 
     return (
         <div className={styles.project_container}>
@@ -44,18 +65,21 @@ function Projeto() {
             </div>
 
             {message && <Message type='success' msg={message} />}
+            {projectMessage && <Message type='success' msg={projectMessage} />}
             <Container customClass="start">
                 {projects.length > 0 &&
                     projects.map((project) => (
-                        <ProjectCard 
-                        id={project.id}
-                        name = {project.name} 
-                        budget = {project.budget}
-                        category = {project.category}
-                        key = {project.key}
-                       
+                        <ProjectCard
+                            id={project.id}
+                            name={project.name}
+                            budget={project.budget}
+                            category={project.category}
+                            key={project.key}
+                            handleRemove={removeProject}
                         />
                     ))}
+                {!removeLoading && <Loading />}
+                {projects.length === 0 && removeLoading && <Message type='warning' msg='Nenhum projeto cadastrado' />}
             </Container>
         </div>
     )
